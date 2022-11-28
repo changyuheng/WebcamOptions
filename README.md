27b1ed2c64cd72d5bbaed2a4b8165d2ba11c8de4
Johann Chang <mr.changyuheng@gmail.com>
2022-11-28 10:13:00 +0800

    Add the first version

---
+++ README.md
@@ -0,0 +1,12 @@
+# WebcamOptions
+
+## Usage
+
+Execute `WebcamOptions.ps1` through any of the following methods:
+
+1. Right click -> Run with PowerShell
+2. Execute it from the PowerShell CLI
+
+## Troubleshooting
+
+Execute `Set-ExecutionPolicy RemoteSigned` from PowerShell if you see "....ps1 cannot be loaded because running scripts is disabled on this system. ..."
---
+++ WebcamOptions.ps1
@@ -0,0 +1,37 @@
+# Execute "Set-ExecutionPolicy RemoteSigned" if you see "....ps1 cannot be loaded because running scripts is disabled on this system. ..."
+
+if (-not (Get-Command -Name "$PSScriptRoot\bin\ffmpeg.exe" 2>$null)) {
+  Write-Error -Message "$PSScriptRoot\bin\ffmpeg.exe is not executable"
+  pause
+  exit 1
+}
+
+$cameras = @()
+
+& $PSScriptRoot\bin\ffmpeg.exe -list_devices true -f dshow -i dummy -hide_banner 2>&1 `
+    | Select-String -Pattern '^\[dshow @ [A-Za-z0-9]+\] "(.*)" \(video\)$' `
+    | Select-Object Matches | ForEach-Object {
+  $cameras += $_.Matches.Groups[1].Value
+}
+
+if (-not ($cameras.Length)) {
+  Write-Error "No camera can be found"
+  pause
+  exit 1
+}
+
+$camera_index = 0
+
+if ($cameras.Length -gt 1) {
+  for ($i = 0; $i -lt $cameras.Length; $i++) {
+    Write-Host "$($i+1)) $($cameras[$i])"
+  }
+  $choice = $(Read-Host -Prompt "Choose the camera # [default=1]")
+  $camera_index = $choice - 1
+  if (-not $choice) {
+    $camera_index = 0
+  }
+}
+
+& $PSScriptRoot\bin\ffmpeg.exe -f dshow -list_options true -i video="$($cameras[$camera_index])" 2>&1
+pause
